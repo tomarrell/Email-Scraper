4e9dd5ca8c258cfb1d52e0a8d97818e4aa459a4d
Tom Arrell <Tom@Toms-MacBook-Pro.local>
2015-09-14 03:30:47 +1200

    Production ready w/o multithreading yet

--- lib/googleSearch.py
+++ lib/googleSearch.py
@@ -8,7 +8,7 @@ class googleSearch():
 	def __init__(self, query):
 		self.query 			= "http://google.com/search?q=" + "+".join(query.split()) + "&num=100&start="
 		self.page 			= 1
-		# self.content 		= requests.get(self.query).text
+		self.content 		= requests.get(self.query).text
 		self.websiteList 	= self.returnWebsiteList(0) + self.returnWebsiteList(100) + self.returnWebsiteList(200)
 
 	def getWebsiteList(self):
@@ -19,15 +19,15 @@ class googleSearch():
 
 	def returnWebsiteList(self, startResult):
 		# PRODUCTION CODE
-		# storeURL = []
-		## Parse raw HTML into BeautifulSoup object
-		# soup = BeautifulSoup(self.content, 'html.parser')
-
 		storeURL = []
-		# Return Google raw HTML
-		rawPageData = open('data/google.txt', 'r').read()
 		# Parse raw HTML into BeautifulSoup object
-		soup = BeautifulSoup(rawPageData, 'html.parser')
+		soup = BeautifulSoup(self.content, 'html.parser')
+
+		# storeURL = []
+		# # Return Google raw HTML
+		# rawPageData = open('data/google.txt', 'r').read()
+		# # Parse raw HTML into BeautifulSoup object
+		# soup = BeautifulSoup(rawPageData, 'html.parser')
 
 		# Loop over cite tags in HTML
 		for cite in soup.find_all("cite"):
--- main.py
+++ main.py
@@ -20,24 +20,40 @@ from lib.checkURLThread import checkURLThread
 
 # Main method
 def main():
+	print(sys.argv)
+	# args = ["law firms", "plumbing auckland"]
 	emailList = []
-	g1 = googleSearch("accounting firms auckland")
-
 	thread1 = checkURLThread("Thread-1")
 	thread2 = checkURLThread("Thread-2")
+	for arg in sys.argv[1:]:
+	# for arg in args:
+		g1 = googleSearch(arg)
+		results = g1.getWebsiteList()
+		for url in results:
+			thread1.insertURL(url)
+			thread1.run()
 
-	thread1.start()
-	thread2.start()
+		# thread2.insertURL(url)
+		# print("Thread 2 Running")
+		# thread2.run()
 
-	for url in g1.getWebsiteList():
-		thread1.insertURL(url)
-		thread1.run()
+	print("===================")
+	print("=       End       =")
+	print("===================")
 
+	# thread1 = checkURLThread("Thread-1")
+	# thread2 = checkURLThread("Thread-2")
+
+	# thread1.start()
+	# thread2.start()
+
+	# for url in g1.getWebsiteList():
+	# 	thread1.insertURL(url)
+	# 	thread1.run()
 
 	# thread1.insertURL("www.allangibson.co.nz")
 	# thread2.insertURL(g1.returnWebsiteList()[1])
 
 	# thread1.start()
-
 
 main()
